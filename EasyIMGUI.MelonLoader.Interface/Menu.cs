5ca2b3a223668d100efe38d963d6a71cd47c59d7
kruumy <joshuampauls@gmail.com>
2023-01-01 17:02:07 -0500

    added ml interface

---
+++ EasyIMGUI.MelonLoader.Interface/Menu.cs
@@ -0,0 +1,26 @@
+﻿using EasyIMGUI.Controls.Base;
+using MelonLoader;
+using System.Collections.Generic;
+
+namespace EasyIMGUI.MelonLoader.Interface
+{
+    public class Menu
+    {
+        public List<Control> Controls { get; } = new List<Control>();
+        public bool IsOpen { get; private set; } = false;
+        public void Open()
+        {
+            MelonEvents.OnGUI.Subscribe(Draw);
+            IsOpen = true;
+        }
+        public void Close()
+        {
+            MelonEvents.OnGUI.Unsubscribe(Draw);
+            IsOpen = false;
+        }
+        private void Draw()
+        {
+            Controls.ForEach(c => c.Draw());
+        }
+    }
+}
