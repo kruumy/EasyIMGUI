7c64474b0ddb65c8e2c31cfbb41b89f3a3138af4
kruumy <joshuampauls@gmail.com>
2023-01-01 17:36:13 -0500

    added protected modifer to buttoncontrol

--- EasyIMGUI.Controls/Base/ButtonControl.cs
+++ EasyIMGUI.Controls/Base/ButtonControl.cs
@@ -7,7 +7,7 @@ namespace EasyIMGUI.Controls.Base
     {
         public GUIContent Content { get; set; } = GUIContent.none;
         public event EventHandler OnButtonPressed;
-        private void Invoke_OnButtonPressed()
+        private protected void Invoke_OnButtonPressed()
         {
             OnButtonPressed?.Invoke(this, EventArgs.Empty);
         }
