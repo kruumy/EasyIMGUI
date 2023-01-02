28a3f63eba26ccf826f2cf63e3f705d509d1d3ca
pauls <pauls@JOSH-PC>
2023-01-02 12:46:34 -0500

    added box and flexiblespace

---
+++ EasyIMGUI.Controls/Box.cs
@@ -0,0 +1,23 @@
+﻿using EasyIMGUI.Controls.Base;
+using EasyIMGUI.Controls.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace EasyIMGUI.Controls
+{
+    public class Box : Control, IContent, IStyle, ILayoutOptions
+    {
+        public GUIContent Content { get; set; } = new GUIContent("");
+
+        public GUIStyle Style { get; set; } // TODO
+
+        public List<GUILayoutOption> LayoutOptions { get; set; } = new List<GUILayoutOption>();
+
+        public override void Draw()
+        {
+            GUILayout.Box(Content, LayoutOptions.ToArray());
+        }
+    }
+}
---
+++ EasyIMGUI.Controls/FlexibleSpace.cs
@@ -0,0 +1,13 @@
+﻿using EasyIMGUI.Controls.Base;
+using UnityEngine;
+
+namespace EasyIMGUI.Controls
+{
+    public class FlexibleSpace : Control
+    {
+        public override void Draw()
+        {
+            GUILayout.FlexibleSpace();
+        }
+    }
+}
