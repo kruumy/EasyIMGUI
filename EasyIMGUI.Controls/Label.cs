bf01f7b472ca7c61751880ac0555a7c55fb18c7d
kruumy <joshuampauls@gmail.com>
2023-01-01 20:14:41 -0500

    added label because i somehow forget it

---
+++ EasyIMGUI.Controls/Label.cs
@@ -0,0 +1,14 @@
+﻿using EasyIMGUI.Controls.Base;
+using UnityEngine;
+
+namespace EasyIMGUI.Controls
+{
+    public class Label : Control, IContent
+    {
+        public GUIContent Content { get; set; } = new GUIContent("");
+        public override void Draw()
+        {
+            GUILayout.Label(Content);
+        }
+    }
+}
