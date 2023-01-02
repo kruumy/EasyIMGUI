e4bc69687c433d7e425f196d7083896ed451ec39
kruumy <joshuampauls@gmail.com>
2023-01-02 09:02:19 -0500

    combined valuecontrol and binded value control

--- EasyIMGUI.Controls/Base/ValueControl.cs
+++ EasyIMGUI.Controls/Base/ValueControl.cs
@@ -9,17 +9,33 @@ namespace EasyIMGUI.Controls.Base
         {
             get
             {
+                if (IsValueBinded) return BindingValueGetter.Invoke();
                 return _Value;
             }
             set
             {
                 if (_Value == null || !_Value.Equals(value))
                 {
+                    if (IsValueBinded) BindingValueSetter.Invoke(value);
                     _Value = value;
                     OnValueChanged?.Invoke(this, value);
                 }
             }
         }
         public event EventHandler<T> OnValueChanged;
+
+        public bool IsValueBinded => BindingValueSetter != null && BindingValueGetter != null;
+        private Func<T> BindingValueGetter;
+        private Action<T> BindingValueSetter;
+        public void Bind(Func<T> getter, Action<T> setter)
+        {
+            BindingValueGetter = getter;
+            BindingValueSetter = setter;
+        }
+        public void Unbind()
+        {
+            BindingValueGetter = null;
+            BindingValueSetter = null;
+        }
     }
 }
