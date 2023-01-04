9e80781ceac5b6fcd621371debb579d97496ae44
kruumy <joshuampauls@gmail.com>
2023-01-03 22:55:55 -0500

    added togglebutton to extra

---
+++ EasyIMGUI.Controls.Extra/ToggleButton.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+namespace EasyIMGUI.Controls.Extra
+{
+    public class ToggleButton : Toggle
+    {
+        public override void Draw()
+        {
+            Value = GUILayout.Toggle(Value, Content, "button", LayoutOptions);
+        }
+    }
+}
