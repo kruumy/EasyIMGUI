edb78115964b26bc86d5757a65db5cb2db06b403
kruumy <joshuampauls@gmail.com>
2023-01-04 09:17:14 -0500

    added a guiskin warpped control

---
+++ EasyIMGUI.Controls/Skin.cs
@@ -0,0 +1,16 @@
+﻿using EasyIMGUI.Controls.Base;
+using UnityEngine;
+
+namespace EasyIMGUI.Controls
+{
+    public class Skin : NestedControl
+    {
+        public GUISkin GUISkin { get; set; } = new GUISkin();
+        public override void Draw()
+        {
+            GUI.skin = GUISkin;
+            base.Draw();
+            GUI.skin = null;
+        }
+    }
+}
